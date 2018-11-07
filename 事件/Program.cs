b50a891f79b8d9c1ccf20d81767352f2e539ef3e
yangdan <yangd@das.com>
2018-11-07 17:01:57 +0800

    添加项目文件。

---
+++ 事件/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 事件
+{
+    public class HumanResoure
+    {
+        //事件处理函数
+        public void SalaryHandler()
+        {
+            Console.WriteLine("好了,我开始计算薪水了，不要急");
+        }
+
+           
+
+
+        static void Main(string[] args)
+        {
+            Employee empl = new Employee();
+            HumanResoure hr = new HumanResoure();
+
+            empl.OnSalary += new SalaryCompute(hr.SalaryHandler);//注册
+            empl.FireEvent();   //触发事件
+
+            Console.ReadKey();
+        }
+    }
+    //声明一个委托类型, 没有参数，也没有返回类型
+    public delegate void SalaryCompute();//
+    public class Employee
+    {
+        public event SalaryCompute OnSalary;
+
+        public virtual void FireEvent()  ///触发事件的方法
+        {
+            if(OnSalary!=null)
+            {
+                OnSalary();
+            }
+        }
+    }
+
+}
---
+++ 事件2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 事件2
+{
+    class HumanResource
+    {
+
+        public void SalaryHandler(object sender,MyEventArgs e)   //事件处理函数， 参数要和委托类型的参数一致
+        {                                                        //必须要一致
+
+            Console.WriteLine("薪水是{0}",e.salary);
+        }
+
+        static void Main(string[] args)
+        {
+            Employee ep1 = new Employee();
+            HumanResource hr = new HumanResource();
+
+            MyEventArgs e = new MyEventArgs(11000);
+            ep1.OnSalaryCompute += new SalaryCompute(hr.SalaryHandler);  ///让事件绑定 事件处理方法   
+
+            ep1.FireEvent(e);    // 触发事件处理函数
+
+            Console.ReadKey();
+        }
+    }
+
+    //定义一个委托类型 。 有参数,无返回类型
+    public delegate void SalaryCompute(object sender, MyEventArgs e);
+
+    public class Employee
+    {
+        //定义一个事件
+        public event SalaryCompute OnSalaryCompute; 
+
+        public virtual void  FireEvent(MyEventArgs e)   ///触发事件的方法
+        {
+            if(OnSalaryCompute!=null)       //如果 事件不为空
+            {
+                OnSalaryCompute(this,e);     //触发事件
+            }
+        }
+
+    }
+
+
+
+
+    //定义事件参数
+    public class MyEventArgs : EventArgs
+    {
+        public readonly double salary;
+        public MyEventArgs(double _salary)
+        {
+            salary = _salary;
+        }
+    }
+}
